7840c61088517cdd45e81547d668d8525238289f
Dnghy-ship <chenyangding670@gmail.com>
2025-03-14 10:22:17 +0800

    change the position of comments

--- Practical4/bmi_cauculator.py
+++ Practical4/bmi_cauculator.py
@@ -1,11 +1,11 @@
 #input weight and height
-#calculate BMI
-#print BMI
-#judge BMI level
 weight=int (input("Enter your weight in kg: "))
 height=float (input("Enter your height in m: "))
+#calculate BMI
 bmi=weight/(height**2)
+#print BMI
 print("Your BMI is: ", bmi)
+#judge BMI level
 if bmi<18.5:
     print("underweight")
 if bmi<=30 and bmi>=18.5:
