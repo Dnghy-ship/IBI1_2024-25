bd569b0a10550f0033b9856cb0a2e6e8395d32c4
Dnghy-ship <chenyangding670@gmail.com>
2025-03-18 16:10:31 +0800

    update program 1

--- Practical5/programming_language_popularity.py
+++ Practical5/programming_language_popularity.py
@@ -15,8 +15,8 @@ import matplotlib.pyplot as plt
 #https://matplotlib.org/stable/gallery/lines_bars_and_markers/bar_colors.html#sphx-glr-gallery-lines-bars-and-markers-bar-colors-py
 fig, ax = plt.subplots()
 #Store the data in the list
-language = ['JavaScript', 'HTML', 'Python', 'SQL', 'TypeScript']
-users = [62.3, 52.9, 51, 51, 38.5]
+language = data.keys()
+users = data.values()
 #we can set the bar color (but I like green)
 #bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:pink']
 #Set the title and x,y axis of the bar plot
