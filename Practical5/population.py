848180829dadb266b13368c176549e68bc58f8a8
Dnghy-ship <chenyangding670@gmail.com>
2025-03-19 13:53:17 +0800

    update program 2

--- Practical5/population.py
+++ Practical5/population.py
@@ -8,35 +8,29 @@ china_sorted=sorted(china)
 print(uk_sorted)
 print(china_sorted)
 
-#create a pie chart for uk
+#create pie charts
 #adapted from https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_and_donut_labels.html
 #Actually I change some variables' names for me to understand
 import matplotlib.pyplot as plt
 import numpy as np
-#Maybe set up the pie chart?
-fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
-#input country name
+# Create a single figure with two subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6), subplot_kw=dict(aspect="equal"))
+
+# Create a pie chart for the UK
+# input country name
 country = ['England','Wales','Northern Ireland','Scotland']
-#input the information into pie chart
-wedges, texts, autotexts = ax.pie(uk, labels=country, autopct='%1.1f%%',textprops=dict(color="w"))
+# input the information into pie chart
+ax1.pie(uk, labels=country, textprops=dict(color="w"))
 #set the legend
-ax.legend(wedges, country,
-          title="Country",
-          loc="center left",
-          bbox_to_anchor=(1, 0, 0.5, 1)
-          )
-#set the format
-plt.setp(autotexts, size=8, weight="bold")
+ax1.legend(country, title="Country", loc="center left", bbox_to_anchor=(0, 0.5, 0, 1))
 #set title
-ax.set_title("UK")
-#I noticed that it print out percentage in the pie chart, but don't know where it was cauculated
-#Some places I haven't understand yet
+ax1.set_title("The UK")
+#Some places I haven't understand yet (those are something about the layout of the chart)
 
 # Create a pie chart for China
-#adapted from https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_and_donut_labels.html
-fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+# adapted from https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_and_donut_labels.html
 # Input data for the pie chart
-wedges, texts = ax.pie(china, wedgeprops=dict(width=0.5), startangle=-40)
+wedges, texts = ax2.pie(china, wedgeprops=dict(width=0.5), startangle=-40)
 # Define annotation properties
 bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
 kw = dict(arrowprops=dict(arrowstyle="-"), bbox=bbox_props, zorder=0, va="center")
@@ -48,7 +42,7 @@ for i, wedge in enumerate(wedges):
     x, y = np.cos(np.deg2rad(angle)), np.sin(np.deg2rad(angle))
     alignment = "right" if x < 0 else "left"
     kw["arrowprops"]["connectionstyle"] = f"angle,angleA=0,angleB={angle}"
-    ax.annotate(
+    ax2.annotate(
         province[i],
         xy=(x, y),
         xytext=(1.35 * np.sign(x), 1.4 * y),
@@ -56,8 +50,10 @@ for i, wedge in enumerate(wedges):
         **kw
     )
 # Set the title for the pie chart
-ax.set_title("China")
-#print them out
+ax2.set_title("China")
+
+# Display both plots together
+plt.tight_layout()
 plt.show()
 
 #I can't fully understand all the steps but I change some variables input and get them right.
