5f319ced2ae8f840de48c59581871d68f84d6064
Dnghy-ship <chenyangding670@gmail.com>
2025-04-25 16:43:58 +0800

    correct practical10

--- Practical10/dalys.py
+++ Practical10/dalys.py
@@ -20,15 +20,6 @@ dalys_data = pd.read_csv("dalys-rate-from-all-causes.csv")
 #Try the function of extacting what we want
 print(dalys_data.iloc[0:10,2])
 #The 10th year with DALYs data recorded in Afghanistan: 
-# 0    1990
-# 1    1991
-# 2    1992
-# 3    1993
-# 4    1994
-# 5    1995
-# 6    1996
-# 7    1997
-# 8    1998
 # 9    1999
 
 #choose the rows (with some help from Copilot)
